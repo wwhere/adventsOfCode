60fe2cddcad892bb52b0f33b80ef335b9bf209ba
wwhere <guillermo.heras@gmail.com>
2025-12-01 07:22:40 +0000

    Day 1

--- 2025/d1a.py
+++ 2025/d1a.py
@@ -2,26 +2,42 @@
 from aoc_tools import read_lines
 
 
-def process(filename):
-    """Main processing for the day"""
-    count = 0
-    list_a = []
-    list_b = []
-
+def process(filename: str, every_click: bool) -> int:
+    solution = 0
+    previous = 50    
+    dial_size = 100
     for line in read_lines(filename):
-        a, b = map(int, line.split())
-        list_a.append(a)
-        list_b.append(b)
+        modifier = int(line[1:]) * (-1 if line[0:1] == "L" else 1)
+        current = previous + modifier
+        if not every_click:
+            if current % dial_size == 0:
+                solution += 1
+        else:
+            solution += abs(current) // dial_size
+            if modifier < 0 and abs(modifier) >= previous and previous > 0:
+                solution += 1
+            current = current % dial_size
+        previous = current
+        continue
 
-    list_a.sort()
-    list_b.sort()
-
-    for a, b in zip(list_a, list_b):
-        count += abs(a-b)
-
-    print(f'{filename}: {count}')
-    return count
+    print(f'{filename} counting all clicks {every_click}: {solution}')
+    return solution
 
 
-assert process('d1a.ex.data') == 11
-process('d1a.data')
+def part_1() -> None:
+    assert process('d1a.1.data', False) == 3
+    process('d1a.2.data', False)
+
+
+def part_2() -> None:
+    assert process('d1a.1.data', True) == 6
+    process('d1a.2.data', True)
+
+
+def main() -> None:
+    part_1()
+    part_2()
+
+
+if __name__ == "__main__":
+    main()
